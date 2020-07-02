7985244132f8d7425c582030ee50a79580b5bb17
JeremYD <delpech.jeremy@hotmail.fr>
2020-07-02 15:15:03 +0200

    JPA: TP 02

--- src/main/java/fr/diginamic/Livre.java
+++ src/main/java/fr/diginamic/Livre.java
@@ -81,6 +81,9 @@ public class Livre {
 		this.auteur = auteur;
 	}
 
+	/**
+	 * toString()
+	 */
 	@Override
 	public String toString() {
 		return "Titre du livre : " + getTitre() + ", Auteur : " + getAuteur();
